69d9ab01a3dd3aa905fe9bf4c7c670abca3b5664
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-05-19 22:53:43 +0530

    changes added

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,24 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("A");
-         int b=0;
+         int b=1;
          int ar[]={1,2,3,4};
           try{                                                      //Exception is parent of all exceptions so it will exceute
              int a=5/b;                                            // Airthmetic Exception
              System.out.println(ar[5]);                           //Out of bound exceptions
              System.out.println("B");  //will not execute
-         }catch(ArrayIndexOutOfBoundsException q){
-//             catch(AirthmaticException E) it will work
-
-//               catch(ArrayIndexOutOfBoundsException) it will not exceutes
-             System.out.println("C");
          }
-          catch (Exception e){
+
+          catch (ArithmeticException e){
               System.out.println("Airthmetic Exception");
           }
+          catch(ArrayIndexOutOfBoundsException q){
+              System.out.println("ArrayIndexOutOfBoundsException");
+          }
+          catch (Exception ex){
+              System.out.println("Parent Exception");
+          }
+
          System.out.println("D");
 
 
