5a9d2faed95d6bb6069d1fba3dbb9ff4f9efa4a8
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-05-21 01:26:29 +0530

    change added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class Main {
     public static void  fun() throws ArithmeticException,ArrayIndexOutOfBoundsException {
         System.out.println("A");
@@ -34,10 +36,16 @@ public class Main {
     public static class A {
         public static void M1(int age){
             try{
-                  ageInfo(15);
-            }catch (AgeInfo e){
-                System.out.println(e.getMessage());
+                  ageInfo(15); //thew exception
+            } catch (AgeInfo e){
+                System.out.println(e.getMessage()+ " Message recieved");
             }
+        }
+
+
+        //  Check Exceptions
+        public static void  m2(int age) throws IOException{   //throws is required
+            if (age < 18) throw new IOException();
         }
     }
 
@@ -65,13 +73,14 @@ public class Main {
          System.out.println("D");
 
           //custom exception
-//          int age=9;
-//          try{
-//            ageInfo(age);
-//        }
-//        catch(RuntimeException r){                   //we can use of AgeInfo Exception
-//            System.out.println(r.getMessage());
-//        }
+          int age=9;
+          try{
+            ageInfo(age);
+        }
+        catch(RuntimeException r){                   //we can use of AgeInfo Exception
+            System.out.println(r.getMessage());
+
+        }
 
 
         A a =new A();
@@ -79,18 +88,25 @@ public class Main {
             a.M1(15);
         }catch (AgeInfo ex){
                System.out.println("Custom Exception");
+            System.out.println("Lets see if it get printed"); //it will not get printed
            }
         catch(ArithmeticException e){
             System.out.println("Airthmetic Exception");
+
         }
         catch (RuntimeException r){
             System.out.println("Runtime Exception");
         }
-        finally {
-            System.out.println("Runtime Exception");
+        finally {                                       //finally will work in all cases even exception is caught or not even if we put return statement in it
+            System.out.println(" Exception");           //finally must written inside try block
+                                                        //
         }
 
+//        try  catch compulsory for this exception
+        try{
+        a.m2(4);
+    }catch (IOException e){
 
-
-    }
+        }
+}
 }
